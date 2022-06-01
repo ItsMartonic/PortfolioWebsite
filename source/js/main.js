b96972b664e8cba1c6791b1e1bf796c63f8b055d
ItsMartonic <OfficialMartonic@gmail.com>
2022-06-01 10:43:20 +1000

    Edit Preloader, Programs Disclaimer & CSS

--- source/js/main.js
+++ source/js/main.js
@@ -5,11 +5,6 @@ tempStyle.innerHTML = "html, body { overflow: hidden; }";
 document.getElementsByTagName("head")[0].appendChild(tempStyle);
 
 $(window).on("load",function(){
-    var min = 1000, max = 5000;
-    var delay = Math.floor(Math.random() * (max - min) + min);
-    var fadeout = 1500;
-    $(".preloader-container").delay(delay).fadeOut(fadeout);
+    $(".preloader-container").fadeOut(500);
     tempStyle.parentNode.removeChild(tempStyle);
 });
-
-// HIDE YOUTUBE EMBED & SCROLLBAR IN PRELOADER
